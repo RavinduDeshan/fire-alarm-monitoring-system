47687b029f8ce33a9a3d6092aff8c94aa35f8c1c
thilina15 <thibbatz@gmail.com>
2023-04-18 20:31:32 +0530

    timeout loop

--- NodeAPI/index.js
+++ NodeAPI/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require('mongoose')
+const { default: axios } = require("axios");
 
 const sensor = require('./senser')
 const app = express();
@@ -17,6 +18,23 @@ const db = mongoose.connection
 db.on('error', error => console.error(error))
 db.once('open', ()=> console.log('connected to mongoose')) 
 
-app.listen(5000,()=>{
+const server = app.listen(5000,()=>{
     console.log("server is runnng....");
 })
+
+server.on("listening", () => {
+    const registerService = () =>
+      axios
+        .put(
+          `${process.env.SERVICEREGISTRYURL}/${process.env.SERVICENAME}/${process.env.SERVICEVERSION}/${process.env.SERVICEPORT}`
+        )
+        .then((res) => {
+          console.log(`Service Registered Key : ${res.data.result}`);
+        })
+        .catch((err) => {
+          console.log(`Error on Registry : ${err}`);
+        });
+  
+    registerService();
+    const interval = setInterval(registerService, 20000);
+  });
